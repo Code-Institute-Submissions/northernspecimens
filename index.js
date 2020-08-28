adf04b74f4418fbc47c8222a636f4281cb555821
Kittencode91 <chelseyrd91@hotmail.com>
2020-08-28 19:20:34 +0000

    js was updated to add animation to a slide show created on the index.html page to showcase the taxidermy pieces.

---
+++ index.js
@@ -0,0 +1,24 @@
+// this javascript allows the burger button and its child elements to expand and show the links//
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
+
+
+//this js code was taken from s3schools to create an automatic slideshow on the home page under "Our Services" https://www.w3schools.com/w3css/w3css_slideshow.asp//
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); 
+}
