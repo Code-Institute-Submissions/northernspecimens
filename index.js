eeea9b42552a295d1a4593e9905e9a988a7c9e71
Kittencode91 <chelseyrd91@hotmail.com>
2020-10-10 19:15:23 +0000

    added functions for toggle buttons

--- index.js
+++ index.js
@@ -59,17 +59,11 @@ function myFunction() {
 }
 
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+function myFunction() {
+  var x = document.getElementById("myDiv");
+  if (x.style.display === "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
